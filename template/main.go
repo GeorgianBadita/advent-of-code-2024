708e29f8d0e91fafb64a2763d8180a9b6839caf1
GeorgianBadita <geo.badita@gmail.com>
2024-11-30 17:39:14 +0000

    Adding template

---
+++ template/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"aoc-2024/datastructures"
+	"bufio"
+	"os"
+)
+
+func readFileAsString(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	lines := []string{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines, nil
+}
+
+const IN_FILE_PATH = "./input.txt"
+
+func main() {
+	data, err := readFileAsString(IN_FILE_PATH)
+
+	if err != nil {
+		println(err)
+		panic("Error reading from " + IN_FILE_PATH)
+	}
+	println(data)
+
+	datastructures.NewPriorityQueue()
+}
