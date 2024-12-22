e694333d490bea677e98c59bc377aad967162e03
GeorgianBadita <geo.badita@gmail.com>
2024-12-22 18:12:14 +0200

    Day 22 part 2

--- day-22-2024/main.go
+++ day-22-2024/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"bufio"
+	"math"
 	"os"
 	"strconv"
 )
+
+type Sequence struct {
+	x1 int
+	x2 int
+	x3 int
+	x4 int
+}
 
 func readFileAsString(filePath string) ([]string, error) {
 	file, err := os.Open(filePath)
@@ -57,6 +65,44 @@ func solvePartOne(nums []uint64) uint64 {
 	return allSum
 }
 
+func solvePartTwo(nums []uint64) int {
+	buyers := [][]int{}
+	for _, num := range nums {
+		buyer := []int{int(num) % 10}
+		for idx := 0; idx < 2000; idx++ {
+			num = getNextSecretNumber(num)
+			buyer = append(buyer, int(num)%10)
+		}
+		buyers = append(buyers, buyer)
+	}
+
+	sequences := map[Sequence]int{}
+	for _, buyer := range buyers {
+		seen := map[Sequence]bool{}
+		for idx := 0; idx < len(buyer)-4; idx++ {
+			slice := buyer[idx : idx+5]
+			a := slice[0]
+			b := slice[1]
+			c := slice[2]
+			d := slice[3]
+			e := slice[4]
+
+			seq := Sequence{b - a, c - b, d - c, e - d}
+			if _, ok := seen[seq]; ok {
+				continue
+			}
+			seen[seq] = true
+			sequences[seq] += e
+		}
+	}
+
+	mx := math.MinInt64
+	for _, res := range sequences {
+		mx = max(mx, res)
+	}
+	return mx
+}
+
 const MOD = 16777216
 
 const IN_FILE_PATH = "./input.txt"
@@ -83,6 +129,6 @@ func main() {
 	if arg == "1" {
 		println(solvePartOne(nums))
 	} else {
-		// placeholder for part 2
+		println(solvePartTwo(nums))
 	}
 }
